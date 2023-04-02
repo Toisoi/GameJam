1f16a60aee45d6430c969f9fbd049070f969de64
Timofey Priguza <63291809+Toisoi@users.noreply.github.com>
2023-04-02 13:17:34 +0300

    menu

--- scenes/sample/game.gd
+++ scenes/sample/game.gd
@@ -9,3 +9,9 @@ class_name Level
 func play_sound(sound: AudioStreamWAV) -> void:
 	audio_player.stream = sound
 	audio_player.play()
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		var image = get_viewport().get_texture().get_image()
+		image.save_png("D://screenshot.png")
